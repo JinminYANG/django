d078ebd4bc26d67b25825a4813739652194069e8
JinminZZang <yangjinmin95@gmail.com>
2022-01-07 17:54:18 +0900

    django DB/template-Javascript Test

--- pragmatic/accountapp/models.py
+++ pragmatic/accountapp/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 import json
 
 # Create your models here.
@@ -6,3 +9,19 @@ class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False)
     team_color = models.CharField(verbose_name='COLOR', max_length=25, default='rgba(240, 116, 137, 1)', null=True)
 
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -9,6 +9,24 @@ from django.views.decorators.csrf import csrf_exempt
 from accountapp.models import HelloWorld
 import json
 
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'accountapp/post_list.html', {'posts': posts})
+
+from django.core.serializers import serialize
+
+import requests
+from django.http import JsonResponse
+
+# In production, this should be set as an environment variable
+API_KEY = 1
+from django.core import serializers
+
 def hello_world(request):
     if request.method == "POST":
         input_text = request.POST.get('hello_world_input')
@@ -20,10 +38,30 @@ def hello_world(request):
         new_hello_world.save()
 
         hello_world_list = HelloWorld.objects.all()
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        res = requests.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        # res_serialized = json.loads(serializers.serialize('json', res))
+
+        context = {
+            'hello_world_list' : HelloWorld.objects.all(),
+            'post_lists' : Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'),
+            # 'json_res' : JsonResponse(res_serialized, safe=False),
+            'res': res,
+        }
+
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        res = requests.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        # res_serialized = json.dumps(serializers.serialize('json', res))
+
+        context = {
+            'hello_world_list': HelloWorld.objects.all(),
+            'posts': Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'),
+            # 'res': json.loads(serializers.serialize('json', res)),
+            'res': res,
+        }
+
+        return render(request, 'accountapp/hello_world.html', context=context)
 
 '''
 @csrf_exempt
@@ -42,7 +80,7 @@ def showmap(request, tbl_person_info_id, TBL_PERSON_INFO=None):
 from .apicodes import keyword
 
 class FormView(generic.View): #formtest
-    template_name = 'apitest/contact.html'
+    template_name = 'acctountapp/contact.html'
 
     def post(self,request):
         inputkw = request.POST.get('name')
@@ -52,4 +90,3 @@ class FormView(generic.View): #formtest
         }
         return render(request,self.template_name,context)
 '''
-
